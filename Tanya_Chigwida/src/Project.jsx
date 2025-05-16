13c97ca6e5138e67d76062f172be1fe3c28fcc2c
“Tanya-abi” <tchigwda6@gmail.com>
2025-05-16 21:05:44 +0200

    Portfolio

--- Tanya_Chigwida/src/Project.jsx
+++ Tanya_Chigwida/src/Project.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Projects() {
   return (
     <>
-      <div>
+      <div id="projects">
         <h1>Projects</h1>
+        <img width="64" height="64" src="https://img.icons8.com/nolan/64/code--v2.png" alt="code--v2"/>
 
         <h2>Developments</h2>
 
@@ -17,10 +18,10 @@ export default function Projects() {
           With a responsive and interactive user interface, effectively 
           fetch and display data from an external API. 
         </p>
-        <a href="https://github.com/Tanya-abi/FE-Capstone-Project_Weather-Dashboard" target="_blank">Githuub link</a>
+        <a href="https://github.com/Tanya-abi/FE-Capstone-Project_Weather-Dashboard" target="_blank"><img width="64" height="64" src="https://img.icons8.com/glyph-neue/64/FFFFFF/github.png" alt="github"/></a> <br />
+        <a href="https://www.figma.com/design/ZZnmnDLbrrmhnkeSdmqTG1/Untitled?node-id=0-1&t=xbO9WzOUKBVP3Pkl-1" target="_blank"><img width="100" height="100" src="https://img.icons8.com/ios-filled/100/FFFFFF/figma--v1.png" alt="figma--v1"/></a>
       </div>
-        
-        
+
 
     </>
   );
